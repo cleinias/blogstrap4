a32d768e2444cd83b1927e4017148b2f933a1810
Stefano Franchi <stefano.franchi@gmail.com>
2017-03-03 14:37:16 -0600

    First commit. Added Bootstrap and Bootswatch css and js files and basic theme files for Grav (.yaml, blueprint, etc.)

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# v0.1.0
+##  3-03-2017
+
+1. [](#new)
+    * First release
+    * ChangeLog started...
---
+++ blogstrap4.php
@@ -0,0 +1,9 @@
+<?php
+namespace Grav\Theme;
+
+use Grav\Common\Theme;
+
+class Blogstrap4 extends Theme
+{
+   
+}
